bd37a23c04456090059ce67c9188f309cc90dc1d
Pramila15 <pramiladalavai@gmail.com>
2021-12-06 22:47:22 +0530

    require working

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,12 @@
-import express from 'express'
+
+import express = require('express')
+import bodyParser = require('body-parser')
+//import bodyParser from 'body-parser';
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const PORT_NO = 1337
 app.get('/',(req,res)=>{
